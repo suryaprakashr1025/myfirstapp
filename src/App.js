cb216c43ab44fb00ccdd7d18e647aff553945415
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-09-30 08:29:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Topbar from './topbar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
